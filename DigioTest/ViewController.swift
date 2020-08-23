e26cf007404760034adb874222f3f1c35e6a0660
Pedro Henrique Borges <pedro.sebastiao@prestador.com.br>
2020-08-23 19:15:39 -0300

    Initial Commit

---
+++ DigioTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DigioTest
+//
+//  Created by Pedro Henrique Borges De Paula Sebastiao (RESOURCE TECNOLOGIA E INFORMATICA LTDA – GEDES – SP) on 23/08/20.
+//  Copyright © 2020 Digio Test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
